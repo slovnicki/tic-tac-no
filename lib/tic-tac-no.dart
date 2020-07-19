2049cad0ff2b4abb5f2b90201a6710684158be75
Sandro Lovnički <sandro@scipack.xyz>
2020-07-19 16:40:44 +0200

    lib/: [remove] Flutter default app and start with Flame

---
+++ lib/tic-tac-no.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flame/game/game.dart';
+
+class TicTacNo extends Game {
+  @override
+  void render(Canvas canvas) {}
+
+  @override
+  void update(double t) {}
+}
